23188b524ea60975c2c1551c3f72d6d219405cde
Costin Dumitrescu <53879882+costindumitrescu@users.noreply.github.com>
2021-12-18 14:54:36 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/people', (req,res) => {
+  const people = [
+    {id: 1, firstName: 'Luke', lastName: 'Skywalker'},
+    {id: 2, firstName: 'Darth', lastName: 'Vader'}
+  ];
+  res.json(people);
+});
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
